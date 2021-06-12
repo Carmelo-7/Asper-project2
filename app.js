ed92e8642b9f1862f8b8cb59ccdf1c4d723b5db2
Suyash <rahatekarsuyash@gmail.com>
2021-06-12 12:42:51 +0800

    added hamburger

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const ham=document.querySelector('.fas');
+const navLinks=document.querySelectorAll('.off');
+
+ham.addEventListener('click',() => {
+    navLinks.forEach((navLink) => {
+        navLink.classList.toggle('off');
+        navLink.classList.toggle('on');
+    });
+   
+
+})
+
